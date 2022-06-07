e05f37426b95c0869425e1608846900f1415d65c
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-06-07 21:27:28 +0300

    Getting unapproved reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -8,7 +8,7 @@ use App\Models\Review;
 class ReviewController extends Controller
 {
     public function getReviews(){
-        $review = Review::all()->where('approved', 1);
+        $review = Review::all()->where('approved', 0);
         return response()->json([
             "success" => true,
             "reviews" => $review
